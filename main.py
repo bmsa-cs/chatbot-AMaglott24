1ea90f0213ef53ae4bcc847fe72d7a1442f75c0b
Alexander Maglott <amaglott24@biomedscienceacademy.org>
2021-01-21 19:19:51 +0000

    I finished the conversation aspect, with asking about favorite character and why they liked the show. I also finished the conclusion and did some troubleshooting with "or"s in If Else statements.

--- main.py
+++ main.py
@@ -46,17 +46,17 @@ def main():
 
   hru = input("Anyways, how are you today? Good, bad, meh? ")
   print("")
-  if hru == "Good":
+  if hru == "Good" or hru =="good":
     print("I'm happy to hear that! I am doing well too.")
-  elif hru == "Bad":
+  elif hru == "Bad" or hru == "bad":
     print("Oh, I'm sorry. Make sure to always reach out to those around you if you need help!")
-  elif hru == "Meh":
-    print("I can understand that. Sometimes life is just life, nothing particularly good or bad about it.")
+  elif hru == "Meh" or hru == "meh":
+    print("I can understand that. Sometimes life is just life, nothing particularly\ngood or bad about it.")
   else:
     print("I don't understand that emotion, but hopefully it is good!")
   shows=input("Have you been enjoying any good shows lately? If so, what show? ")
   print("")
-  if shows == "The Mandalorian":
+  if shows == "The Mandalorian" or shows == "The Office" or shows == "Wanda Vision":
     print("That's cool, I've been enjoying that too!")
   elif shows == "no":
     print("Oh, ok. Not everyone enjoys TV shows!")
@@ -69,29 +69,27 @@ def main():
     else:
       showchoice = "The Mandalorian" 
     print("Awesome! I have been enjoying " +showchoice+ " lately.")
-    
+  favchar = input("What is your favorite character from the show?: ")
+  if favchar == "The Mandalorian":
+    print("That is my favorite character too!")
+  else:
+    print("Nice!")
+  print("")
+  why = input("Is there any particular reason that you liked this show? ")
+
+  why = random.randint(1,3)
+  if why == 1:
+    print("Those are some good reasons, maybe I will go and watch it!")
+    print("\nWell, it was nice talking, "+str(name)+"! I'm going to go watch that show!")
+  elif why == 2:
+    print("I probably won't watch that, it doesn't seem like my kind of show.\nSounds fun though!")
+    print("\nIt was nice talking, "+str(name)+"! I have to go now though. Have a nice day!")
+  else: 
+    print("That sounds like a must see!")
+    print("\nWell, it was nice talking, "+str(name)+"! I'm going to go watch that movie!")
+
 if __name__ == "__main__":
   main()
   t = input("Run pytest? (y/n)").lower()
   if t == 'y':
     run_tests()
-
-  """
-  Question ideas:
-  Introduction:
-    - What is your name?
-    - What is your prefered gender?
-    - What is your age?
-  Discussion:
-    - How are you?
-    - Are there any TV Shows you have been enjoying lately?
-      - Answers: "Awesome! I have been enjoying (insert show)!" or "That's cool, I've been enjoying that too!"
-    - Do you have a favorite character from that show?
-      - Answers: "That is a cool character." or "Its always good to like many characters."
-    - What is your favorite movie?
-      -"I've never seen that before.""
-      Why did you like that movie?
-      -"Those are some good reasons, maybe I will go and watch it" or "I probably won't watch that, it doesn't seem like my kind of movie." or "That sounds like a must see!"
-  Conclusion:
-    "Well, it was nice talking, (Name)! I'm going to go watch that movie." or "It was nice talking, (Name)! I have to go now though. Have a nice day!"
-  """
